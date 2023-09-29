73a9a5044940bdd0f31fd2e785bce748108f9d47
Mirzo <Mirzo@.com>
2023-09-30 01:50:59 +0900

    Feat:MongoDb ulanish

---
+++ app.js
@@ -0,0 +1,52 @@
+console.log("For test");
+// 2023 09 20
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const fs = require("fs");
+const db = require("./server").db();
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("Error:",err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
+
+// MONGODB connect
+
+
+
+// #1.Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // htmldan yozilganlarni expreswga kkiritmaydi,agar buni yozmasak.
+
+//#2 sesssion code
+
+// #3.Backendda html ni yasaymiz.viwes code.
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user })
+});
+
+
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+
+
+// #4.Routinglar.routing code
+app.post('/create-item', (req, res) => {
+  console.log(req);
+  res.json({test: "success"});
+})
+
+app.get("/", function (req,res) {
+  res.render("reja")
+});
+// module.exports = app;
+module.exports = app;
--- server.js
+++ server.js
@@ -1,54 +1,27 @@
-console.log("For test");
-// 2023 09 20
+const http = require('http');
 
-const express = require("express");
+const mongodb = require("mongodb")
 
-const app = express();
-const http = require("http");
-const fs = require("fs");
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("Error:",err);
-  } else {
-    user = JSON.parse(data)
-  }
-});
+// for MongoDB
+let db;
+const connectionString = "mongodb://atlas-sql-6515584055fa2b0a683e929b-oip7v.a.query.mongodb.net/Reja?ssl=true&authSource=admin"
 
-
-// #1.Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // htmldan yozilganlarni expreswga kkiritmaydi,agar buni yozmasak.
-
-//#2 sesssion code
-
-// #3.Backendda html ni yasaymiz.viwes code.
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user })
-});
-
-
-
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-
-
-// #4.Routinglar.routing code
-app.post('/create-item', (req, res) => {
-  console.log(req);
-  res.json({test: "success"});
-})
-
-app.get("/", function (req,res) {
-  res.render("reja")
-});
-
-
-const server = http.createServer(app);
+mongodb.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, (err, client) => {
+  if (err) console.log("Error connection MongoDB");
+  else {
+    console.log("DataBase MongoDB Connection succed");
+    module.exports = client;
+    const app = require("./app");
+    const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
   console.log(`the server is running succesfully on port ${PORT},http://localhost:${PORT}`);
 });
+  }
+})
+
+
+

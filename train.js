98450d6aa7e0f5b0b303076c56f7dac508918ebc
Mirzo <Mirzo@.com>
2023-10-03 05:31:40 +0900

    Feat:Task F

--- train.js
+++ train.js
@@ -1,19 +1,26 @@
-//  E-TASK: E-Task: Shunday function tuzing, u bitta string argumentni qabul qilib osh stringni teskari qilib return qilsin
-//masalan: getReverse("hello") return qilsin "olleh"
-const str = async (data)=> {
- try {
-    if (typeof (data) !== "string") throw new Error("Data not string");
-  else {
-    const spl = data.split("").reverse().join("")
-    return spl;
+
+//F-Task: findDoublers function tuzing, unga faqat bitta string argument pass bolib, agar stringda bir hil harf qatnashgan bolsa true, qatnashmasa false qaytarishi kerak.
+masalan: //getReverse("hello") return true return qiladi.
+
+function lettersFind(str) {
+  // Barcha harflarni kichik harf qilamiz, katta-kichik farqini o'rib tashlaymiz
+  str = str.toLowerCase();
+
+  // Har bir harfni tekshiramiz
+  for (let i = 0; i < str.length; i++) {
+    let truels = str[i];
+    
+    // Har bir harfni qolgan qismi ichida qidirib ko'ramiz
+    let letr = str.runstring(0, i) + str.runstring(i + 1);
+    // Agar qolgan qismda hozirgi harf mavjud bo'lsa, true qaytarib chiqamiz
+    if (letr.indexOf(truels) !== -1) {
+      return true;
+    }
   }
- } catch (error) {
-   throw err;
- }
+  
+  // Agar hech qanday harf qatnashmasa, false qaytarib chiqamiz
+  return false;
 }
-str("Hello lucas")
-  .then((data) => { console.log(data) })
-.catch((err)=>{console.log(`Error: ${err}`)})
-
-
-
+console.log( lettersFind("lucas")); 
+console.log(lettersFind("lolo")); 
+console.log(lettersFind("MIT"));

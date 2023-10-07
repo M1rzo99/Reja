41e0733af797bcbc02d7ce4745ce43c296556e1a
Mirzo <Mirzo@.com>
2023-10-07 20:00:23 +0900

    Fix:Task-H

---
+++ train.js
@@ -0,0 +1,11 @@
+function revStr(e) {
+  const revedStr = e.split('').reverse().join('');
+  return revedStr;
+}
+const text = "ABC",
+ strAylantir = revStr(text);
+console.log(strAylantir); 
+
+
+
+
